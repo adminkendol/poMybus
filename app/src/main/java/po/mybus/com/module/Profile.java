75e5f54599e5ab6d7830ae221a5fa30a97bfa392
adminkendol <chandra.comment@gmail.com>
2018-02-21 10:59:48 +0700

    #lastpost

---
+++ app/src/main/java/po/mybus/com/module/Profile.java
@@ -0,0 +1,55 @@
+package po.mybus.com.module;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import po.mybus.com.MainActivity;
+import po.mybus.com.R;
+
+/**
+ * Created by Chandra on 17/02/2018.
+ */
+
+public class Profile extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView status_toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.back);
+        status_toolbar = (TextView) findViewById(R.id.status_toolbar);
+        status_toolbar.setText("Profile");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            Log.d("NAV","BACK");
+            Intent intent;
+            intent = new Intent(Profile.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
